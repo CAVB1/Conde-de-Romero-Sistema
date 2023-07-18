3179aa8e48c32d0f3112dfd8446dbc0b49c2a342
Carlos Villegas <2030072@upt.edu.mx>
2023-07-17 23:27:30 -0600

    Clase para operaciones con grupos

---
+++ db/grupos.php
@@ -0,0 +1,27 @@
+<?php
+
+class Grupos{
+    public static function InsertGrupo($nombre_grupo){
+        $sql="INSERT INTO grupos(nombre_grupo)
+        VALUES('$nombre_grupo')";
+        include_once 'db_connection.php';
+
+        return $conexion->query($sql);
+
+    }
+
+    public static function getAllGrupos(){
+        return consultaSQL("SELECT * FROM grupos");
+    }
+
+    public static function getGrupo($id){
+        return consultaSQL("SELECT * FROM grupos WHERE id_grupo=$id");
+    }
+
+    public static function deleteGrupo($id){
+        include_once 'db_connection.php';
+        $sql="DELETE FROM grupos WHERE id_grupo=$id";
+        return $conexion->query($sql);
+    }
+}
+?>
