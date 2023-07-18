acb6aea7aee81d6d6bd9396de570a143e8ca2645
Carlos Villegas <2030072@upt.edu.mx>
2023-07-17 23:26:51 -0600

    Función para hacer consultas

---
+++ db/quey.php
@@ -0,0 +1,27 @@
+<?php
+function consultaSQL($sql) {
+    include "db_connection.php";
+
+    $conn = $conexion;
+
+    if (!$conn) {
+        die("Error al conectar a la base de datos: " . mysqli_connect_error());
+    }
+
+    $resultado = mysqli_query($conn, $sql);
+
+    if (!$resultado) {
+        die("Error al ejecutar la consulta: " . mysqli_error($conn));
+    }
+
+    $datos = array();
+
+    while ($fila = mysqli_fetch_assoc($resultado)) {
+        $datos[] = $fila;
+    }
+
+    mysqli_close($conn);
+
+    return $datos;
+}
+?>
