61ce04e30104d0c352eae1f3a23e0c0b393a0add
Carlos Villegas <2030072@upt.edu.mx>
2023-08-09 23:58:40 -0600

    Registrar alumnos individualmente

--- Administrador/javascript/alumnos.js
+++ Administrador/javascript/alumnos.js
@@ -2,8 +2,17 @@ document.body.onload = function () {
     const select1 = document.getElementById("gruposGral");
     const select2 = document.getElementById("grupoSelectInd");
     const btnGral = document.getElementById("btnGral");
+    const btnInd = document.getElementById("btnInd");
 
     const archivo = document.getElementById("file");
+
+    const matriculaInput = document.getElementById("matriculaInput");
+    const nombreInput = document.getElementById("nombreInput");
+    const apellidopInput = document.getElementById("apellidopInput");
+    const apellidomInput = document.getElementById("apellidomInput");
+    const contraInput = document.getElementById("contrasenaInput");
+
+
 
     fetch('../db/apis/grupos_formated.php')
         .then(response => response.json())
@@ -79,41 +88,80 @@ document.body.onload = function () {
                     // Access elements using row.nombre, row.edad, etc.
                     // console.log( `Row ${rowIndex}: ${JSON.stringify(rowData)}\n`);
                     // console.log( `Aña: ${JSON.stringify(rowData)}\n`);
-                    if(rowData['Matricula']!==undefined&&rowData['Contrasena']!==undefined&&rowData['Nombre']!==undefined&&rowData['Apellido Paterno']!==undefined&&rowData['Apellido Materno']!==undefined){
-                        let formData=new FormData();
-                        formData.append('matricula',rowData['Matricula']);
-                        formData.append('passwd',rowData['Contrasena']);
-                        formData.append('nombre',rowData['Nombre']);
-                        formData.append('apellidop',rowData['Apellido Paterno']);
-                        formData.append('apellidom',rowData['Apellido Materno']);
-                        formData.append('idgrupo',id_grupo);
+                    if (rowData['Matricula'] !== undefined && rowData['Contrasena'] !== undefined && rowData['Nombre'] !== undefined && rowData['Apellido Paterno'] !== undefined && rowData['Apellido Materno'] !== undefined) {
+                        let formData = new FormData();
+                        formData.append('matricula', rowData['Matricula']);
+                        formData.append('passwd', rowData['Contrasena']);
+                        formData.append('nombre', rowData['Nombre']);
+                        formData.append('apellidop', rowData['Apellido Paterno']);
+                        formData.append('apellidom', rowData['Apellido Materno']);
+                        formData.append('idgrupo', id_grupo);
 
-                        fetch('../db/apis/agregar_alumno.php',{
-                            method:'POST',
-                            body:formData
+                        fetch('../db/apis/agregar_alumno.php', {
+                            method: 'POST',
+                            body: formData
                         })
-                        .then(response=>response.json())
-                        .then(data=>{
-                            
-                        })
-                        .catch(error=>{
+                            .then(response => response.json())
+                            .then(data => {
 
-                        });
-                        
+                            })
+                            .catch(error => {
 
-                    }else{
+                            });
+
+
+                    } else {
                         console.error("Error");
+                        alert("Ha ocurrido un error");
+                        setTimeout(function () { window.location.href = "alumnos.html" }, 200);
                         return;
                     }
-                    
+
 
                 }
 
-                
+                alert("Proceso terminado");
+                setTimeout(function () { window.location.href = "alumnos.html" }, 200);
 
             };
 
             reader.readAsArrayBuffer(file);
+
+        }
+    });
+
+    btnInd.addEventListener("click", function () {
+        if (matriculaInput.value.trim() !== "" && nombreInput.value.trim() && apellidopInput.value.trim() !== "" && apellidomInput.value.trim() !== "" && contraInput.value.trim() !== "" && select2.value.trim() !== "") {
+            let matricula = matriculaInput.value;
+            let nombre = nombreInput.value;
+            let apellidop = apellidopInput.value;
+            let apellidom = apellidomInput.value;
+            let contra = contraInput.value;
+            let idgrupo = select2.value;
+
+            let formData = new FormData();
+            formData.append('matricula', matricula);
+            formData.append('passwd', contra);
+            formData.append('nombre', nombre);
+            formData.append('apellidop', apellidop);
+            formData.append('apellidom', apellidom);
+            formData.append('idgrupo', idgrupo);
+
+            fetch('../db/apis/agregar_alumno.php', {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => response.json())
+                .then(data => {
+                    alert("Registro completado");
+                    setTimeout(function () { window.location.href = "alumnos.html" }, 500)
+                })
+                .catch(error => {
+                    alert("Ha ocurrido un error\nIntentalo de nuevo")
+                });
+        } else {
+            alert("Asegurate de llenar todos los campos");
+            return;
         }
     });
 
