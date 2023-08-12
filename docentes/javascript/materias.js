6dd01e87c8445c78bd0a896af89850f1b3ef51f5
Carlos Villegas <2030072@upt.edu.mx>
2023-08-11 22:34:01 -0600

    API para mostrar materias en interfaz de docente

---
+++ docentes/javascript/materias.js
@@ -0,0 +1,22 @@
+document.body.onload= setTimeout(function(){
+    
+const matContainer=document.getElementById("materias-cont");
+console.log(`../db/apis/materias_docente.php?nocontrol=${userID}`);
+fetch(`../db/apis/materias_docente.php?nocontrol=${userID}`)
+.then(response=>response.json())
+.then(data=>{
+    data.forEach(element => {
+        let card=document.createElement("div");
+        card.setAttribute("class","card");
+
+        card.innerHTML=`<label >Grupo</label> <br><input type="text" placeholder="Grupo" value="${element.Grupo}"><label >Materia</label><br> <input type="text" placeholder="Materia" value="${element.nombre_materia}"><button onclick="window.location.href=\'calificaciones.html?id_materia=${element.id_materias}\'">Entrar</button>`;
+        // card.innerHTML=`añañaña`;
+
+        matContainer.appendChild(card);
+    });
+})
+.catch(error=>{
+    console.error("AÑA");
+});
+
+},1000); 
