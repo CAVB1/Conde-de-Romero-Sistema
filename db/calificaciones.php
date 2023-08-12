57a7b7faa37bedce952d2d3d5954da013ad5f799
Carlos Villegas <2030072@upt.edu.mx>
2023-08-12 01:28:06 -0600

    Guardado de rubros de materias por alumnos

--- db/calificaciones.php
+++ db/calificaciones.php
@@ -2,15 +2,15 @@
 include_once 'quey.php';
 
 class Calificaciones{
-    public static function InsertRubro($nombre_rubro,$id_materia,$valor, $bloque){
-        include_once 'db_connection.php';
+    public static function InsertRubro($nombre_rubro,$id_materia,$id_alumno,$valor, $bloque){
+        require 'db_connection.php';
 
-        $stmsql="INSERT INTO rubros(nombre_rubro, fk_id_materia,valor,bloque) VALUES('$nombre_rubro',$id_materia,$valor,'$bloque')";
+        $stmsql="INSERT INTO rubros(nombre_rubro, fk_id_materia,fk_id_alumno,valor,bloque) VALUES('$nombre_rubro',$id_materia,$id_alumno, $valor,'$bloque')";
         return $conexion->query($stmsql);
     }
 
     public static function getNoRubros($id_materia){
-        return consultaSQL("SELECT COUNT(rubros.id_rubros) as no_rubros FROM rubros WHERE rubros.fk_id_materia=$id_materia")[0]['no_rubros'];
+        return consultaSQL("SELECT COUNT(rubros.id_rubro) as no_rubros FROM rubros WHERE rubros.fk_id_materia=$id_materia")[0]['no_rubros'];
     }
 }
 ?>
--- db/materias.php
+++ db/materias.php
@@ -7,6 +7,10 @@ class Materias{
         $stmsql="INSERT INTO materias(nombre_materia,fk_id_docente,fk_id_grupo) VALUES('$nombre',$docente,$grupo)";
         return $conexion->query($stmsql);
     }
+
+    public static function getListaAlumnos($id_materia){
+        return consultaSQL("select alumnos.* from alumnos join grupos on alumnos.fk_id_grupo=grupos.id_grupo join materias on materias.fk_id_grupo=grupos.id_grupo where materias.id_materias=$id_materia");
+    }
 }
 
 ?>
